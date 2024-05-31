73cb40079ebaa3ae40a207f841182f3edd223159
matternstsr <matternstsr@gmail.com>
2024-05-31 09:24:55 -0500

    nope

--- _sh.c
+++ _sh.c
@@ -12,51 +12,26 @@ c_dt_t comm_data;
 
 int main(int argc, char **argv)
 {
-    size_t input_length = 0;
-    int prompt_value = 0;
-    int exit_status = 0; /* Initialize exit status variable */
+	size_t input_length = 0;
+	int prompt_value = 0;
 
-    (void)argc;
-    prog.prog_name = argv[0];
-
-    while (1)
-    {
-        comm_data.op_ind = 0, comm_data.op_add = 0, comm_data.op_ct = 0;
-        comm_data.cmd_ind = 0, comm_data.cmd_ct = 0;
-        signal(SIGINT, handle_interrupt);
-        signal(SIGTSTP, handle_interrupt);
-        prompt_value = prompt("Shellshocked:# ", &comm_data.input, &input_length);
-        if (prompt_value < 0)
-            continue;
-        shell_cracked(comm_data.input);
-        run_comm(comm_data.comms);
-        /* Check if any command failed */
-        exit_status = check_exit_status(comm_data.comms);
-        free_comm_data();
-        fflush(stdout);
-    }
-
-    return exit_status; /* Return the exit status */
-}
-
-/**
- * check_exit_status - Checks the exit status of commands
- * @comms: The linked list of commands
- * 
- * This function iterates through the linked list of commands and
- * checks the exit status of each command. If any command has a non-zero
- * exit status, it returns that status. If all commands have a zero exit
- * status, it returns zero.
- * 
- * Return: The exit status of commands
- */
-int check_exit_status(c_lst_t *comms)
-{
-    while (comms)
-    {
-        if (comms->exit_status != 0)
-            return comms->exit_status;
-        comms = comms->next;
-    }
-    return 0; /* No command failed, return success */
+	(void)argc;
+	prog.prog_name = argv[0];
+	
+	while (1)
+	{
+		comm_data.op_ind = 0, comm_data.op_add = 0, comm_data.op_ct = 0;
+		comm_data.cmd_ind = 0, comm_data.cmd_ct = 0;
+		signal(SIGINT, handle_interrupt);
+		signal(SIGTSTP, handle_interrupt);
+		prompt_value = prompt("Shellshocked:# ", &comm_data.input, &input_length);
+		if (prompt_value < 0)
+			continue;
+		shell_cracked(comm_data.input);
+		run_comm(comm_data.comms);
+		free_comm_data();
+		fflush(stdout);
+	}
+	
+	return (0);
 }
